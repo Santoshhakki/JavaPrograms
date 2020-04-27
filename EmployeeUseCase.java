94a42e7b348c5ce370b5b19e95738bf769343282
SirsikarAkshay <akshaysirsikar@gmail.com>
2020-04-27 12:07:46 +0530

    Monthly wage of employee calculated

--- EmployeeUseCase.java
+++ EmployeeUseCase.java
@@ -6,6 +6,8 @@ public class EmployeeUseCase{
 		double fullDay=8;
 		double halfDay=4;	
 		double empWage;
+		double monthDays=20;
+		
 		double empCheck=Math.floor(Math.random() * 10 ) %2;
 		System.out.println("Welcome to the Employee Use Case problem");
 		
@@ -13,21 +15,26 @@ public class EmployeeUseCase{
 		case 1:
 			int IS_FULLTIME=1;
 			double fullTimeCheck=Math.floor(Math.random()*10)%2;
-
+			double totalMonthWage;
 			System.out.println("Employee is present");
 
 			switch ((int) fullTimeCheck){
 			case 1:
 				empWage=wagePerHour*fullDay;
+				totalMonthWage=empWage*monthDays;
 				System.out.println("Employee works fulltime and Employee wage is: "+ empWage);
+				System.out.println("Employee wage for a month is: "+ totalMonthWage);
 
 			break;
 
-			default:
+			case 0:
 				empWage=wagePerHour*halfDay;
+				totalMonthWage=empWage*monthDays;
                                 System.out.println("Employee works parttime and Employee wage is: "+ empWage);
+				System.out.println("Employee wage for a month is: "+ totalMonthWage);
 			break;
 			}
+		break;
 
 		case 0:
 			System.out.println("Employee is absent");
